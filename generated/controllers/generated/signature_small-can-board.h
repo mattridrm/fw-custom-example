0c05e24170583afbcab7c595fce59a008d442570
GitHub git update Action <action@github.com>
2026-04-26 01:51:28 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/signature_small-can-board.h
+++ generated/controllers/generated/signature_small-can-board.h
@@ -2,5 +2,5 @@
 // CHeaderConsumer was generated automatically by rusEFI tool config_definition-all.jar based on gen_config.sh by SignatureConsumer
 //
 
-#define SIGNATURE_HASH 1961199521
-#define TS_SIGNATURE "rusEFI main.2026.04.25.small-can-board.1961199521"
+#define SIGNATURE_HASH 3133350752
+#define TS_SIGNATURE "rusEFI main.2026.04.26.small-can-board.3133350752"
