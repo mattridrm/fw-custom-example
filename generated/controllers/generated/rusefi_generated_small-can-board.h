3ad627c561bafc4e7143af97186af5f360e859af
GitHub git update Action <action@github.com>
2026-07-01 02:38:15 +0000

    Custom Auto-generated Configs
    [no ci]

--- generated/controllers/generated/rusefi_generated_small-can-board.h
+++ generated/controllers/generated/rusefi_generated_small-can-board.h
@@ -2237,7 +2237,7 @@
 #define ts_show_wbo_canbus_index true
 #define ts_show_wbo_canbus_set_index true
 #define ts_show_wbo_canbus_set_type false
-#define TS_SIGNATURE "rusEFI main.2026.06.30.small-can-board.783705749"
+#define TS_SIGNATURE "rusEFI main.2026.07.01.small-can-board.783705749"
 #define TS_SIMULATE_CAN '>'
 #define TS_SIMULATE_CAN_char >
 #define TS_TEST_COMMAND 't'
--- generated/controllers/generated/signature_small-can-board.h
+++ generated/controllers/generated/signature_small-can-board.h
@@ -3,4 +3,4 @@
 //
 
 #define SIGNATURE_HASH 783705749
-#define TS_SIGNATURE "rusEFI main.2026.06.30.small-can-board.783705749"
+#define TS_SIGNATURE "rusEFI main.2026.07.01.small-can-board.783705749"
